4c7550b29686171eed8f4f7c42948988b2bfd176
MoreKodhek <morekodhek@gmail.com>
2022-10-10 09:43:31 +0300

    Simple python projects

---
+++ Madlib/madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "More Argwings"
+# print("Subscribe to " + youtuber)
+# print("Subscribe to " +youtuber)
+# print(f"Subscribe to {youtuber} ")
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+    I love to {verb}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
