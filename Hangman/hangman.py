0bcdac97f02798e4056475ec374fbecd5b7efe12
More Argwings Kodhek <morekodhek@gmail.com>
2023-02-28 13:21:31 +0300

    Defining the variables and pulling from words file

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,2 +1,14 @@
 import random
 from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(word) #randomly picks a word from the list
+    while '-' in word or ' '  in word:
+        return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() 
