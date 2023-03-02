dec231188fa23d1fd74a308f1b0366c6d845135d
More Argwings Kodhek <morekodhek@gmail.com>
2023-03-02 12:01:18 +0300

    Working on user input

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -11,4 +11,15 @@ def hangman():
     word = get_valid_word(words)
     word_letters = set(word) # letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() 
+    used_letters = set() # what the user has guessed
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used that letter')
+
+    user_input = input('Type something: ')
+    print(user_input)
