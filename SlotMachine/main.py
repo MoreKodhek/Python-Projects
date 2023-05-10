6b1dce52a908d62406594dd03264a06af14a82ec
More Argwings Kodhek <morekodhek@gmail.com>
2023-05-10 16:51:21 +0300

    fixing actual slot machine print direction

--- SlotMachine/main.py
+++ SlotMachine/main.py
@@ -1,6 +1,47 @@
+import random
+
 MAX_LINES = 3  # global constant
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):  # the actual "slot machine"
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        col = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(colums):  # printing from horizontal to vertical
+    for row in range(len(colums[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
 
 
 def deposit():
@@ -35,6 +76,7 @@ def get_number_of_lines():
 
 
 def get_bet():  # checking if the bet is between the minimum and maximum bet amount
+
     while True:
         amount = input('How much would you like to bet one each line? KES ')
         if amount.isdigit():
@@ -50,11 +92,19 @@ def get_bet():  # checking if the bet is between the minimum and maximum bet amo
     return amount
 
 
-def main():
+def main():  # call all functions
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount, your current balance is: KES {balance}")
+        else:
+            break
+
     print(
         f'You are betting KES {bet} on {lines} lines. Total bet is equal to: KES {total_bet}')
     print(balance, lines)
