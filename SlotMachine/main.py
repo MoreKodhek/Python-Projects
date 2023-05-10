4bb98a908d6402fd63edc69e4bf60eb7ce32ac3c
More Argwings Kodhek <morekodhek@gmail.com>
2023-05-10 11:09:28 +0300

    created get lines function

--- SlotMachine/main.py
+++ SlotMachine/main.py
@@ -1,6 +1,9 @@
+MAX_LINES = 3
+
+
 def deposit():
     while True:
-        amount = input('How much are you depositing? $ ')
+        amount = input('How much are you depositing? KES ')
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -11,3 +14,26 @@ def deposit():
             print('Please enter a number')
 
     return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines = input(
+            'Enter the number of lines to bet on (1-' + str(MAX_LINES) + ')? ')
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print('Enter a valid number of lines')
+        else:
+            print('Please enter a number')
+
+    return lines
+
+
+def main():
+    balance = deposit()
+
+
+main()
